f83e62f9b5fff13189da4e666dbb3825f755591d
ahmetyazicii <83782637+ahmetyazicii@users.noreply.github.com>
2021-05-27 22:20:04 +0300

    Update Deneme.java

--- Deneme.java
+++ Deneme.java
@@ -2,5 +2,15 @@ Class Deneme{
   public static void main(String[] args)
     System.out.println("deneme1");
     System.out.println("deneme2");
+  System.out.println("deneme2");
+  System.out.println("deneme2");
+  System.out.println("deneme2");
+  System.out.println("deneme2");
+  System.out.println("deneme2");
+  System.out.println("deneme2");
+  System.out.println("deneme2");
+  System.out.println("deneme2");
+  System.out.println("deneme2");
+  System.out.println("deneme2");
   }
 }
