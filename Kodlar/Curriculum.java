33d84c0c4aa7717741398ec1f5646f4141b9c775
Samet Nalbant <57832048+samoo00@users.noreply.github.com>
2021-06-06 13:32:31 +0300

    Update Curriculum.java

--- Kodlar/Curriculum.java
+++ Kodlar/Curriculum.java
@@ -2,28 +2,27 @@ import java.util.ArrayList;
 
 public class Curriculum {
 	private ArrayList<Course> courseList;
-    private String department;
-
-    Curriculum(){
-    	courseList = new ArrayList<>();
-    }
-    public void addCourse(Course newCourse){
-    	courseList.add(newCourse);
-    }
-    public String getDepartment(){
-        return department;
-    }
-    public void setDepartment(String department){
-    	this.department = department;
-    }
-    
-    @Override
-    public String toString() {
-    	String temp = "";
-    	temp += "Department: " + department + "\n" + "Term\tCourse Code\tCourse Name\t\t\tCredit\n" ;
-    	for(int i=0;i<courseList.size();i++) {
-    		temp += courseList.get(i).getTerm() + "\t" + courseList.get(i).getCourseCode()+ "\t" + courseList.get(i).getCourseName() +"\t\t" + courseList.get(i).getCredit() +"\n";
+	private String department;
+	Curriculum(){
+    		courseList = new ArrayList<>();
     	}
-    	return temp;
-    }	
+    	public void addCourse(Course newCourse){
+    		courseList.add(newCourse);
+    	}
+    	public String getDepartment(){
+        	return department;
+    	}
+    	public void setDepartment(String department){
+    		this.department = department;
+    	}
+    
+    	@Override
+    	public String toString() {
+    		String temp = "";
+    		temp += "Department: " + department + "\n" + "Term\tCourse Code\tCourse Name\t\t\tCredit\n" ;
+    		for(int i=0;i<courseList.size();i++) {
+    			temp += courseList.get(i).getTerm() + "\t" + courseList.get(i).getCourseCode()+ "\t" + courseList.get(i).getCourseName() +"\t\t" + courseList.get(i).getCredit() +"\n";
+    		}
+    		return temp;
+    	}	
 }
