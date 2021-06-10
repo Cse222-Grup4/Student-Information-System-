22e254a7e4fd3fd78fbd5b5e2f06095b9d2c79ee
Serdil Anıl Ünlü <58235498+serdilanil@users.noreply.github.com>
2021-06-10 12:28:57 +0300

    Update DataBase.java

--- Kodlar/DataBase.java
+++ Kodlar/DataBase.java
@@ -32,8 +32,8 @@ public class DataBase {
         }
     }
 
-	
-	// course kisminda sikinti var.Courselarin tum ozelliklerini yazmak lazim ya da farkli bir course dosyasi olusturmak.
+    
+    // course kisminda sikinti var.Courselarin tum ozelliklerini yazmak lazim ya da farkli bir course dosyasi olusturmak.
     private void readTeachersFile() throws IOException {
         // reader for read teachers file
         File usersFile = new File(System.getProperty("user.dir"),teachersFilePath);
@@ -53,32 +53,109 @@ public class DataBase {
             Teacher teacher = new Teacher(userMail,userPassword,userName,userSurname,userID,this);
 
             // reading teacher's courses
-		// hata var simdilik yorum satirinda
+        // hata var simdilik yorum satirinda
            /*
-	   courseIDLine = line.split(";")[5];
+       courseIDLine = line.split(";")[5];
             if(!courseIDLine.equals("null")) {
                 courseIDs = courseIDLine.split(",");
                 for (String c : courseIDs)
                     teacher.addCourse(c);
             }
-	    */
+        */
             teachers.add(teacher);
         }
         bufferedReader.close();
+    }
+
+    public boolean readCourses(String filename){
+        try{
+            FileReader myObj = new FileReader(filename);
+            BufferedReader bufferedReader = new BufferedReader(myObj);
+            String line;
+
+            while((line = bufferedReader.readLine()) != null){
+                String parts[] = line.split(";");
+                courses.add(new Course(Integer.parseInt(parts[0]), parts[1], parts[2], Integer.parseInt(parts[3])));
+            }
+        }catch(IOException io){
+            System.out.println("IOException occurred when reading 'teachers.txt' file.The exception is printing: ");
+            io.printStackTrace();
+        }
+        return true;
+    }
+
+    public boolean readStudentFile(){
+        // reader for read teachers file
+        try{
+            FileReader myObj = new FileReader("students.txt");
+            BufferedReader bufferedReader = new BufferedReader(myObj);
+
+            String line;
+
+            while((line = bufferedReader.readLine()) != null){
+                /*HashMap<String, int> tempAttendance = new HashMap();
+                HashMap<String, Grade> tempGrades = new HashMap();*/
+
+                String parts[] = line.split(";");
+                Student tempStudent = new Student(parts[2], parts[3], parts[0], parts[1], Integer.parseInt(parts[4]), Integer.parseInt(parts[6]));
+                tempStudent.setAdvisorTeacherID(Integer.parseInt(parts[5]));
+                tempStudent.setTerm(Integer.parseInt(parts[7]));
+                tempStudent.setDepartment(parts[8]);
+                tempStudent.setCourseSelectionApprove(Boolean.parseBoolean(parts[9]));
+                String tempCourses[] = parts[10].split(",");
+                for(int i = 0;i < tempCourses.length;i++){
+                    String tempCurrentCourse[] = tempCourses[i].split("\\.");
+                    for(int j = 0;j < courses.size();j++){
+                        if(courses.get(j).getCourseCode().equals(tempCurrentCourse[i])){
+                            tempStudent.getCurrentCourses().add(courses.get(j));
+                            break;
+                        }
+                    }
+                    tempStudent.getAttendance().put(tempCurrentCourse[0], Integer.parseInt(tempCurrentCourse[1]));
+                    tempStudent.getGrades().put(tempCurrentCourse[0], new Grade(Integer.parseInt(tempCurrentCourse[2]), Integer.parseInt(tempCurrentCourse[3]), Integer.parseInt(tempCurrentCourse[4])));
+
+                    /*tempAttendance.put(tempCurrentCourse[0],tempCurrentCourse[1]);
+                    tempGrades.put(tempCurrentCourse[0], new Grade(Integer.parseInt(tempCurrentCourse[2]), Integer.parseInt(tempCurrentCourse[3]), Integer.parseInt(tempCurrentCourse[4])));*/
+                }
+
+                tempCourses = parts[11].split(":");
+                for(int i = 0;i < tempCourses.length;i++){
+                    String tempCurrentCourses[] = tempCourses[i].split(",");
+
+                    for(int t = 0;t < tempCurrentCourses.length;t++){
+                        String tempCurrentCourse[] = tempCurrentCourses[t].split("\\.");
+                        for(int j = 0;j < courses.size();j++){
+                            if(courses.get(j).getCourseCode().equals(tempCurrentCourse[0])){
+                                tempStudent.getPastCourses().get(i).add(courses.get(j));
+                                tempStudent.getGrades().put(tempCurrentCourse[0], new Grade(Integer.parseInt(tempCurrentCourse[1])));
+                                break;
+                            }
+                        }
+                    }
+                }
+                students.add(tempStudent);
+            }
+            bufferedReader.close();
+            myObj.close();
+        }catch(IOException io){
+        System.out.println("IOException occurred when reading 'teachers.txt' file.The exception is printing: ");
+        io.printStackTrace();
+        }
+        return true;
     }
     
     public void displayTeachers(){
         for(Teacher t : teachers)
             System.out.println(t);
     }
-	public Curriculum getCurriculum(String department) {
-		for(int i=0;i<curriculums.size();i++) {
-			if(curriculums.get(i).getDepartment().equals(department)) {
-				 return curriculums.get(i);
-			}
-		}
-		return null;
-	}
+    public Curriculum getCurriculum(String department) {
+        for(int i=0;i<curriculums.size();i++) {
+            if(curriculums.get(i).getDepartment().equals(department)) {
+                 return curriculums.get(i);
+            }
+        }
+        return null;
+    }
 
 
 
