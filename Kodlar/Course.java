d33e6c050ef8d74680c176fa085f7763bd71120a
Serdil Anıl Ünlü <58235498+serdilanil@users.noreply.github.com>
2021-06-10 12:28:18 +0300

    Update Course.java

--- Kodlar/Course.java
+++ Kodlar/Course.java
@@ -1,38 +1,43 @@
 import java.util.ArrayList;
 
 public class Course {
-    private Grade grade;
-    private ArrayList<Attendance> absent;
+    //private Grade grade;
+    // private ArrayList<Attendance> absent;
+    private int courseTerm;
     private String courseCode;
     private String courseName;
     private int credit;
-    private int numOfAttendance = 0;
-    private int numOfAbsent = 0;
-    private boolean ísPass = false;
-    private int term;
     private ArrayList<Student> students;
 
-    public Course(String courseCode, String courseName, int credit,int term) {
+    private final int TotalCourses = 30;
+   
+    /*private int numOfAttendance = 0;
+    private int numOfAbsent = 0;
+    private boolean ísPass = false;
+    */
+
+    public Course(int courseTerm, String courseCode, String courseName, int credit) {
         this.courseCode = courseCode;
         this.courseName = courseName;
         this.credit = credit;
-        this.term = term;
-        absent = new ArrayList<>();
-        grade = new Grade();
+        this.courseTerm = courseTerm;
+        /*absent = new ArrayList<>();
+        grade = new Grade();*/
     }
-
+/*
     public Grade getGrade() {
         return grade;
     }
     public void setGrade(Grade grade) {
         this.grade = grade;
-    }
+    }*/
+    /*
     public ArrayList<Attendance> getAbsent() {
         return absent;
     }
     public void setAbsent(ArrayList<Attendance> absent) {
         this.absent = absent;
-    }
+    }*/
     public String getCourseCode() {
         return courseCode;
     }
@@ -50,13 +55,7 @@ public class Course {
     }
     public void setCredit(int credit) {
         this.credit = credit;
-    }
-    public int getTerm(){
-        return term;
-    }
-    public int setTerm(int term){
-        this.term = term;
-    }
+    }/*
     public int getNumOfAttendance() {
         return numOfAttendance;
     }
@@ -68,16 +67,14 @@ public class Course {
     }
     public void setNumOfAbsent(int numOfAbsent) {
         this.numOfAbsent = numOfAbsent;
-    }
+    }*/
 
-    public String getLetterGrade()
+    public String getLetterGrade(int totalGrade, int numOfAbsent)
     {
-        double ratio = (double)numOfAbsent / numOfAttendance;
+        double ratio = (double)numOfAbsent / TotalCourses;
 
         if (ratio > 0.3)
             return "NA";
-
-        int totalGrade = grade.getTotalGrade();
 
         if (totalGrade < 55)
             return "FF";
@@ -97,6 +94,7 @@ public class Course {
             return "AA";
     }
 
+    /*                                      // artık öğrenciyi bulup yapması lazım
     public void addGrade(int grade,int type){
         switch (type){
             case 1 -> this.grade.setMidtermGrade(grade);
@@ -104,19 +102,16 @@ public class Course {
             case 3 -> this.grade.setProjectGrade(grade);
         }
     }
-
+*/
 
 
     @Override
     public String toString() {
         return "CourseS{" +
-                "grade=" + grade +
-                ", absent=" + absent +
                 ", courseCode='" + courseCode + '\'' +
                 ", courseName='" + courseName + '\'' +
-                ", credit=" + credit +
-                ", numOfAttendance=" + numOfAttendance +
-                ", numOfAbsent=" + numOfAbsent +
+                ", credit=" + credit + '\'' +
+                ",  courseTerm=" +  courseTerm +
                 '}';
     }
 }
