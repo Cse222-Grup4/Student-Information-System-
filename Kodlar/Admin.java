592284b365261d70b0a98fbf9b51922eca9a6774
bekici <45313360+berkcanekici@users.noreply.github.com>
2021-06-22 22:00:21 +0300

    Update Admin.java

--- Kodlar/Admin.java
+++ Kodlar/Admin.java
@@ -1,98 +1,110 @@
 import java.util.*;
 import java.io.*;
 
-public class Admin extends Officer{
+public class Admin extends Officer {
 	private DataBase db;
-	Admin(String name, String surname, String mail, String password, int ID,DataBase db) {
-		this.name=name;
-		this.surname=surname;
-		this.mail=mail;
-		this.password=password;
-		this.ID=ID;
-		this.db=db;
+
+	Admin(String name, String surname, String mail, String password, int ID, DataBase db)
+	{
+
+		super(name, surname, mail, password, ID, db);
 	}
 	/**
-	* Constructor with two parameters.
-	* @param mail Admin's e-mail address.
-	* @param password Admin's password.
-	*/
-	Admin(String mail, String password) {
+	 * Constructor with two parameters.
+	 * @param mail Admin's e-mail address.
+	 * @param password Admin's password.
+	 */
+	Admin(String mail, String password)
+	{
 		super(mail, password);
 	}
-	public void print(){
+
+	public void print()
+	{
+		//...
 	}
 	/**
-	* Inquire teacher information by Teacher ID
-	* @param teachers Teacher List
-	* @param teacherID Teacher's ID to inquire
-	* @return Teacher object
-	*/
-	public Teacher inquireTeacherInformation(ArrayList <Teacher> teachers, int teacherID) {
-		for (int i = 0; i < teachers.size(); i++) {
-		if (teachers.get(i).getUserID() == teacherID)
-		return teachers.get(i);
-		}
+	 * Inquire teacher information by Teacher ID
+	 * @param teachers Teacher List
+	 * @param teacherID Teacher's ID to inquire
+	 * @return Teacher object
+	 */
+	public Teacher inquireTeacherInformation(ArrayList <Teacher> teachers, int teacherID)
+	{
+		for (int i = 0; i < teachers.size(); i++)
+			if (teachers.get(i).getUserID() == teacherID)
+				return teachers.get(i);
+
 		return null;
 	}
 
 	/**
-	* Inquire officer information by Officer ID
-	* @param officers Officer List
-	* @param officerID Officer's ID to inquire
-	* @return Officer object
-	*/
-	public Officer inquireOfficerInformation(ArrayList < Officer > officers, int officerID) {
-		for (int i = 0; i < officers.size(); i++) {
-		if (officers.get(i).getUserID() == officerID)
-		return officers.get(i);
-		}
+	 * Inquire officer information by Officer ID
+	 * @param officers Officer List
+	 * @param officerID Officer's ID to inquire
+	 * @return Officer object
+	 */
+	public Officer inquireOfficerInformation(ArrayList < Officer > officers, int officerID)
+	{
+		for (int i = 0; i < officers.size(); i++)
+			if (officers.get(i).getUserID() == officerID)
+				return officers.get(i);
+
 		return null;
 	}
 
-	public boolean addOfficer(ArrayList < Officer > officers , Officer newOfficer){
+	public boolean addOfficer(ArrayList < Officer > officers , Officer newOfficer)
+	{
 		// Registers officer by using Array List's add method.
 		boolean success;
 		success = officers.add(newOfficer);
 		return success;
 	}
 
-	public boolean addTeacher(ArrayList < Teacher > teachers , Teacher newTeacher){
+	public boolean addTeacher(ArrayList < Teacher > teachers , Teacher newTeacher)
+	{
 		// Registers teacher by using Array List's add method.
 		boolean success;
 		success = teachers.add(newTeacher);
 		return success;
 	}
 
-	public boolean addStudent(ArrayList < Student > students , Student newStudent){
+	public boolean addStudent(ArrayList < Student > students , Student newStudent)
+	{
 		// Registers student by using Array List's add method.
 		boolean success;
 		success = students.add(newStudent);
 		return success;
 	}
 
-	public boolean removeOfficer(ArrayList < Officer > officers , Officer oldOfficer){
+	public boolean removeOfficer(ArrayList < Officer > officers , Officer oldOfficer)
+	{
 		// Registers teacher by using Array List's add method.
 		boolean success;
 		success = officers.remove(oldOfficer);
 		return success;
 	}
 
-	public boolean removeTeacher(ArrayList < Teacher > teachers , Teacher oldTeacher){
+	public boolean removeTeacher(ArrayList < Teacher > teachers , Teacher oldTeacher)
+	{
 		// Registers teacher by using Array List's add method.
 		boolean success;
 		success = teachers.remove(oldTeacher);
 		return success;
 	}
 
-	public boolean removeStudent(ArrayList < Student > students , Student oldStudent){
+	public boolean removeStudent(ArrayList < Student > students , Student oldStudent)
+	{
 		// Registers student by using Array List's add method.
 		boolean success;
 		success = students.remove(oldStudent);
 		return success;
 	}
 
-	public void menu(){
-		int choice , innerChoice,id;
+	public void menu()
+	{
+		int choice, innerChoice, id;
+
 		for (;;) {
 			System.out.println("WELCOME TO ADMIN MENU");
 			System.out.println("0) Exit");
@@ -104,87 +116,92 @@ public class Admin extends Officer{
 			choice = scanner.nextInt();
 
 			switch (choice) {
-
 				case 0:
-				return;
+					return;
 				case 1:
 					System.out.println("0) Exit");
 					System.out.println("1) Add Officer");
 					System.out.println("2) Add Student");
 					System.out.println("3) Add Teacher");
+
 					innerChoice = scanner.nextInt();
 					String mail,password, name, surname;
 					int year;
-					System.out.println("Enter name");
-					name; = scanner.nextLine();
-					System.out.println("Enter surname");
+
+					System.out.println("Enter name: ");
+					name = scanner.nextLine();
+
+					System.out.println("Enter surname: " );
 					surname = scanner.nextLine();
-					System.out.println("Enter id");
+
+					System.out.println("Enter id: ");
 					id = scanner.nextInt();
-					System.out.println("Enter mail");
+
+					System.out.println("Enter mail: ");
 					mail = scanner.nextLine();
-					System.out.println("Enter password");
+
+					System.out.println("Enter password: ");
 					password = scanner.nextLine();
-					switch (innerChoice){
+
+					switch (innerChoice) {
 						case 0:
-						break;
+							break;
 						case 1:
-							Officer tempO = new Officer(name, surname, mail, password, id,db);
+							Officer tempO = new Officer(name, surname, mail, password, id, db);
 							//add database the Officer.
-						break;
+							break;
 						case 2:
 							System.out.println("Enter year");
 							year = Integer.parseInt(scanner.nextLine());
 							Student tempS = new Student(mail, password, name, surname, id, year);
 							//add database the student.
-						break;
+							break;
 						case 3:
 							Admin tempA = new Admin(name, surname, mail, password, id,db);
 							//add database the Admin.
-						break;
+							break;
 						default:
-						break;
+							break;
 					}
-				break;
+					break;
 				case 2:
-					
 					System.out.println("0) Exit");
 					System.out.println("1) Remove User");
 					innerChoice = scanner.nextInt();
-					switch (innerChoice){
+
+					switch (innerChoice) {
 						case 0:
-						break;
+							break;
 						case 1:
 							System.out.println("Enter id");
 							id = scanner.nextInt();
 							//remove func database
-						break;
+							break;
 						default:
-						break;
+							break;
 					}
-				break;
+					break;
 				case 3:
-					
 					System.out.println("0) Exit");
 					System.out.println("1) Inquire User");
 					innerChoice = scanner.nextInt();
-					switch (innerChoice){
+
+					switch (innerChoice) {
 						case 0:
-						break;
+							break;
 						case 1:
 							System.out.println("Enter id");
 							id = scanner.nextInt();
-							
+
 							//inquire func database
-						break;
+							break;
 						default:
-						break;
+							break;
 					}
-				break;
+					break;
 				default:
-				System.out.println("Tekrar giris");
+					System.out.println("Tekrar giris");
 			}
 		}
 	}
-
 }
