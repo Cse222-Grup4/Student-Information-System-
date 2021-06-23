7fc9573f12b065e529b69d396c99cc9718343fc6
Cse222-Grup4 <82971050+Cse222-Grup4@users.noreply.github.com>
2021-06-23 14:58:14 +0300

    Update DataBase.java

--- Kodlar/DataBase.java
+++ Kodlar/DataBase.java
@@ -132,7 +132,7 @@ public class DataBase {
     public void importOfficersFromFile() throws NumberFormatException, IOException {
         String line;
         BufferedReader br = new BufferedReader(new FileReader(officersFilePath));
-
+	br.readLine();//first line
         while ((line = br.readLine()) != null) {
             officers.add(new Officer(
                     line.split(";")[3],
