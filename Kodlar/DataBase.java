f1f68e7b464b6106e714b4f225abb5865755f4dc
Samet Nalbant <57832048+samoo00@users.noreply.github.com>
2021-06-25 20:49:30 +0300

    Update DataBase.java

--- Kodlar/DataBase.java
+++ Kodlar/DataBase.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
-
+import java.util.PriorityQueue;
 public class DataBase {
 
     private ArrayList<Student> students;
