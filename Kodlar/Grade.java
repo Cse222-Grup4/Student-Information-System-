7e6e7a80f84db487a7721902770d8226cc8cc0de
bekici <45313360+berkcanekici@users.noreply.github.com>
2021-06-25 18:13:52 +0300

    Update Grade.java

--- Kodlar/Grade.java
+++ Kodlar/Grade.java
@@ -1,53 +1,140 @@
+/**
+ * Grade class for represent the students grade.
+ */
 public class Grade {
+    /**
+     * Midterm grade of student.
+     */
     private int midtermGrade = 0;
+
+    /**
+     * Final grade of student.
+     */
     private int finalGrade = 0;
+
+    /**
+     * Project grade of student.
+     */
     private int projectGrade = 0;
+
+    /**
+     * Total grade of student.
+     */
     private int totalGrade = 0;
 
+    /**
+     * Default ctor.
+     */
     Grade(){}
 
-    Grade(int midtermGrade, int finalGrade, int projectGrade){
+    /**
+     * Constructor with parameter.
+     * @param totalGrade Total grade of student.
+     */
+    Grade(int totalGrade)
+    {
+        this.totalGrade = totalGrade;
+    }
+
+    /**
+     * Constructor with 3 parameter.
+     * @param midtermGrade Midterm grade of student.
+     * @param finalGrade Final grade of student.
+     * @param projectGrade Project grade of student.
+     */
+    Grade(int midtermGrade, int finalGrade, int projectGrade)
+    {
         this.midtermGrade = midtermGrade;
         this.finalGrade =  finalGrade;
         this.projectGrade = projectGrade;
     }
 
-    Grade(int totalGrade){
-        this.totalGrade = totalGrade;
-    }
-
-    // getters and setters
-    public int getMidtermGrade() {
+    /**
+     * Get midterm grade of student.
+     * @return Midterm grade of student.
+     */
+    public int getMidtermGrade()
+    {
         return midtermGrade;
     }
-    public void setMidtermGrade(int midtermGrade) {
+
+    /**
+     * Set midterm of student.
+     * @param midtermGrade Midterm grade of student.
+     */
+    public void setMidtermGrade(int midtermGrade)
+    {
         this.midtermGrade = midtermGrade;
     }
-    public int getFinalGrade() {
+
+    /**
+     * Get final grade of student.
+     * @return Final grade of student.
+     */
+    public int getFinalGrade()
+    {
         return finalGrade;
     }
-    public void setFinalGrade(int finalGrade) {
+
+    /**
+     * Set final grade of student.
+     * @param finalGrade Final grade of student.
+     */
+    public void setFinalGrade(int finalGrade)
+    {
         this.finalGrade = finalGrade;
     }
-    public int getProjectGrade() {
+
+    /**
+     * Get project grade of student.
+     * @return Project grade of student.
+     */
+    public int getProjectGrade()
+    {
         return projectGrade;
     }
-    public void setProjectGrade(int projectGrade) {
+
+    /**
+     * Set project grade of student.
+     * @param projectGrade Project grade of student.
+     */
+    public void setProjectGrade(int projectGrade)
+    {
         this.projectGrade = projectGrade;
     }
+
+    /**
+     * Get total grade of student.
+     * @return Total grade of student.
+     */
     public int getTotalGrade()
     {
         totalGrade = (int)((0.4 * midtermGrade) + (0.5 * finalGrade) + (0.1 * projectGrade));
         return totalGrade;
     }
+
+    /**
+     * Set total grade of student.
+     * @param totalGrade Total grade of student.
+     */
+    public void setTotalGrade(int totalGrade)
+    {
+        this.totalGrade = totalGrade;
+    }
+
+    /**
+     * Get past total grade of student.
+     * @return Past total grade of student.
+     */
     public int getPastTotalGrade()
     {
         return totalGrade;
     }
-    public void setTotalGrade(int totalGrade){
-        this.totalGrade = totalGrade;
-    }
 
+    /**
+     * toString method of Grade class.
+     * @return String of grade.
+     */
     public String toString()
     {
         return "Midterm: " + midtermGrade + " Final: " + finalGrade + " Project: " + projectGrade +
